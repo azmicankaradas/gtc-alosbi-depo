aa3f736ab6df0490c2e3d5b3cebcd127f91bcd37
azmicankaradas <azmicankaradas96@gmail.com>
2026-03-05 17:11:52 +0300

    added pwa logo for ios and android

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,8 @@ export default function RootLayout({
   return (
     <html lang="tr" className="dark">
       <head>
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         <div className="pb-20 md:pb-0">
